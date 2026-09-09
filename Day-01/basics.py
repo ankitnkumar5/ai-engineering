a9cb225fb1f5c2dfa1207fca3021b0aa65660469
ankitnkumar5 <anxkit5@gmail.com>
2026-09-09 21:01:27 +0530

    print , variables

---
+++ Day-01/basics.py
@@ -0,0 +1,42 @@
+print("Hello World")
+print("Hello Python")
+print("My Name is Ankit")
+print("I am learning AI Engineer")
+
+
+#python can do calculations also 
+print(10+20) #calculation of addition
+print("10+20")  # it will print the text only 
+
+
+#Q1) Write a program that displays this:-
+#My AI Engineering Journey
+#Name: Ankit
+#Goal: AI Engineer
+#Day: 1
+#Language: Python
+#I am starting my 300-day journey today!
+
+print("My AI Engineering Journey")
+print("Name: Ankit")    
+print("Goal: AI Engineer")
+print("Day: 1")
+print("Language: Python")
+print("I am starting my 300-day journey today!")
+
+
+#VARIABLES
+
+name = "Ankit"
+age = 21
+goal = "AI Engineer"
+
+print(name)
+print(age)
+print(goal)
+
+print("My name is ",name)
+print("I am ",age," years old")
+print("My goal is to become an ", goal)
+
+
