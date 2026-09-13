35a035bde078759e1c8b4074086d1e4921832938
ankitnkumar5 <anxkit5@gmail.com>
2026-09-13 16:55:54 +0530

    practice of 3 question realted to input() function

---
+++ Day-01/input_practice.py
@@ -0,0 +1,34 @@
+#------Question 1: Personal Information-------------
+#Write a program that asks for:
+#Name
+#Age
+#City
+#--Then prints:
+#My name is Ankit
+#I am 21 years old
+#I live in Faridabad
+
+#--------------ANSWER:---------
+#name = input("Enter your name: ")
+#age = input("Enter your Age:")
+#city = input("Enter your City:")
+#print("My namr is"),name
+#print("I am",age,"years old")
+#print("I live in",city)
+
+#--------------Question 2: Add Two Numbers-------------
+#--Ask the user for two numbers and print:Addition: 30
+
+#----------Answer:---------
+#a = int(input("Enter first number:"))
+#b = int(input("Enter second number:"))
+#print("Addition:",a+b)
+ 
+
+#------------Question 3: Calculate Area of Rectangle-------------------
+#--Ask the user for:Length and Width  (Formula:Area = length × width)
+
+#----------Answer:---------
+#Length = float(input("Enter length of rectangle:"))
+#Breadth = float(input("Enter breadth of rectangle:"))
+#print("Area of rectangle is:",Length*Breadth)
